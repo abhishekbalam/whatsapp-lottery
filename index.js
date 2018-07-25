1f24d9af97e5fd956056bc007bf9c1f44af8cb24
Abhishek Balam <abhishekbalam96@gmail.com>
2018-07-25 23:52:07 +0530

    Wed Jul 25 21:45:53 IST 2018

---
+++ index.js
@@ -0,0 +1,26 @@
+document.getElementById("result").onclick = function(){
+  var data=document.getElementById("data").value.trim();
+  data = data.split(":").pop().split("\n");
+  for(var i=0;i<data.length;i++){
+    if(data[i]==""){
+      data.splice(i,1);
+    }
+    data[i]=data[i].split(".").pop().trim();
+  }
+  console.log(data);
+  var result=[];
+  var max=3;
+  while(result.length<max){
+    var x = Math.floor((Math.random() * data.length));
+    if(result.indexOf(x)<0){
+      result.push(x);
+    }
+  }
+  console.log(result);
+  var html="<h3 style='text-align:center'>The Winners are:</h3><br><ul style='text-align:justify'>";
+  for(var i=0;i<result.length;i++){
+    html=html+"<li>"+data[result[i]]+"</li>";
+  }
+  html=html+"</ul>";
+  document.getElementById("final").innerHTML=html;
+}
